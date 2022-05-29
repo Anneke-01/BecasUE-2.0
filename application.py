5479beee5f7d38ab9de974dfefd4fba4eb8bbc13
Anneke-01 <anneke_paulina11@hotmail.com>
2022-05-29 02:16:47 -0600

    :lipstick: Adding login template

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
 from bd_connection import connection
 from call_tables import calltables
+from flask import Flask, session, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     connection()
     calltables()
+    app.run(port=3300)
