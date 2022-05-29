3a880e2f83e22b0ecd6a063477fc6c3b7d091598
Anneke-01 <anneke_paulina11@hotmail.com>
2022-05-28 21:46:28 -0600

    🌱 Adding the main file and another one to call the tables on db

---
+++ application.py
@@ -0,0 +1,6 @@
+from bd_connection import connection
+from call_tables import calltables
+
+if __name__ == '__main__':
+    connection()
+    calltables()
---
+++ call_tables.py
@@ -0,0 +1,15 @@
+from bd_connection import connection
+
+c = connection()
+def calltables():
+    try:
+        with c.cursor() as cursor:
+            users = cursor.execute("SELECT * FROM Usuario").fetchall()
+            list = []
+            for i in users:
+                list.append(i)
+            
+            for f in list:
+                print(f[1:5])
+    except Exception as e:
+        print("Ocurrió un error al consultar: ", e) 
